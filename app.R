ba3df9bb13df2bdc2282885bc6297d290e69427b
poglodjan <100129357+poglodjan@users.noreply.github.com>
2023-06-07 02:44:53 +0200

    Update app.R

--- app.R
+++ app.R
@@ -12,6 +12,8 @@ library(stringr)
 data <- read.csv("ViewingActivity.csv")
 exploring <- data %>% mutate(Profile.Name = ifelse(Profile.Name=="Magda Papierz",
                                                    "Magda", Profile.Name)) %>%
+  mutate(Country = ifelse(Country=="KR (Korea, Republic of)",
+                               "KR (Korea)", Country)) %>%
   mutate(Profile.Name = ifelse(Profile.Name=="Pasożyt", "Janek", Profile.Name)) %>%
   mutate(Duration = str_sub(Duration,end=-4)) %>%
   mutate(hours = as.numeric(substr(Duration,1,2)),
@@ -54,7 +56,9 @@ ui <- fluidPage(
         margin: 0 auto;
       }
       .distPlot{
+        font-size:16px;
         width: 700px;
+        margin-top:20px;
         margin: 0 auto;
         border: 1px solid silver; 
         background: white; padding: 4px;
@@ -62,13 +66,23 @@ ui <- fluidPage(
         box-shadow: 1px 1px 3px silver; 
       }
       .tabelka{
+        font-size:16px;
+        width: 300px;
+        margin: 0 auto;
+        border-radius: 8px;
+      }
+      .tabelka2{
+        font-size:16px;
         width: 200px;
         margin: 0 auto;
         border-radius: 8px;
       }
       .slupkowy{
+        font-size:16px;
         color: black;
         width:400px;
+        padding-right:20px;
+        height: 490px;
         margin: 0 auto;
         border: 1px solid silver; 
         background: white; padding: 4px;
@@ -77,42 +91,45 @@ ui <- fluidPage(
       }
       
       .mapka{
-        color: black;
-        width: 500px;
-        height: 600px;
+        padding-top:50px;
+        width: 640px;
+        height: 450px;
         margin: 0 auto;
-        margin-top: 40px;
+        margin-top:50px;
         border: 1px solid silver; 
         background: white; padding: 4px;
         border-radius: 8px;
         box-shadow: 1px 1px 3px silver; 
-        text-align: center;
       }
+      
       .okno1{
+      font-size:16px;
       margin: 0 auto;
       width:800px;
-      margin-top: 20px;
+      padding-top: 20px;
       margin-bottom: 20px;
-      height:520px;
-      border: 4px solid white; 
+      height:540px;
+      border: 8px solid white; 
       }
       
       .okno2{
+      font-size:16px;
       margin: 0 auto;
-      width:800px;
+      width: 1200px;
       margin-top: 20px;
-      margin-bottom: 20px;
-      height: 480px;
-      border: 4px solid white; 
+      margin-bottom: 40px;
+      height: 500px;
+      border: 8px solid white; 
       }
       
       .okno3{
+      font-size:16px;
       margin: 0 auto;
-      width:800px;
-      margin-top: 20px;
+      width: 1200px;
+      margin-top: 60px;
       margin-bottom: 20px;
-      height: 700px;
-      border: 4px solid white; 
+      height: 600px;
+      border: 8px solid white; 
       }
       
     ")
@@ -158,6 +175,7 @@ ui <- fluidPage(
       column(6, 
              div(
                class = "tabelka",
+               HTML("<br>"),
                textOutput("opis1"),
                HTML("<br>"),
                tableOutput("tabelka")
@@ -177,17 +195,27 @@ ui <- fluidPage(
     ),
     
     fluidRow(
-      column(12,
+      div(class="okno3",
+        fluidRow(
+          
+      column(4,
+                    div(
+                      class="tabelka2",
+                      HTML("<br><br>"),
+                      "Kraje w których były oglądane filmy:",
+                      HTML("<br><br>"),
+                      tableOutput("tabelka2")
+                    )
+             ),
+      column(8,
              div(
-               class="okno3",
-             div(
-             class="mapka",
-             HTML("<br><br>"),
-             "Kraje w których były oglądane filmy:",
-             plotOutput("mapka")
+               class="mapka",
+               plotlyOutput("mapka")
              )
-             )
-  ))
+          )
+      ),
+      )
+      )
   
   ))
   ))
@@ -200,11 +228,15 @@ server <- function(input, output) {
                                         ,exploring$Year >= input$Zakres[1],
                                         exploring$Year <= input$Zakres[2]), 
             x = ~datetime, y=~NewDuration,
-              type = "bar") %>%
+              type = "bar",marker=list(color="red")) %>%
         layout(yaxis=list(range=c(0,8000), title = "Czas oglądania w minutach"),
                xaxis = list(title = "Oś czasu"),
-               title = paste0("Ile czasu ", input$Osoba, " spędzał/a na netflixie w latach ",
-                              input$Zakres[1], "-", input$Zakres[2]))
+               title = list(text = 
+                              paste0("Ile czasu ", input$Osoba, " spędzał/a na netflixie w latach ",
+                                          input$Zakres[1], "-", input$Zakres[2]), 
+                            font = list(color = "white")),
+               plot_bgcolor = "black",
+               paper_bgcolor = "black")
     })
     
     output$tabelka <- renderTable({
@@ -215,7 +247,7 @@ server <- function(input, output) {
           count(Title) %>%
           filter(Profile.Name == input$Osoba) %>%
           arrange(desc(n)) %>%
-          head(6) %>% select(Title)
+          head(8) %>% select(Title)
           t
     })
     
@@ -224,14 +256,18 @@ server <- function(input, output) {
         exploring$Year >= input$Zakres[1],
         exploring$Year <= input$Zakres[2],
         Profile.Name == input$Osoba)
-      suma_filmow <- sum(res$licznik_filmow)
-      suma_seriali <- sum(res$licznik_serialow)
-      plot_ly(labels = c("Film", "Serial"), values = c(suma_filmow, suma_seriali), 
-              type = "pie")
+      suma_filmow <- sum(res$licznik_filmow)/(sum(res$licznik_filmow)+sum(res$licznik_serialow))
+      suma_seriali <- sum(res$licznik_serialow)/(sum(res$licznik_filmow)+sum(res$licznik_serialow))
+      df <- data.frame(Film = (suma_filmow*100), Serial = (suma_seriali*100))
+      
+      pl <- plot_ly(data=df, x=~names(df), y=~unlist(df))
+      pl %>% add_trace(type="bar",marker=list(color="red")) %>%
+        layout(plot_bgcolor = "black", paper_bgcolor = "black", xaxis=list(title="Rodzaj"),
+               yaxis=list(title="Liczba w procentach"))
     })
     
     output$opis1 <- renderText({
-    paste0("Sześć najpopularniejszych filmów i seriali dla użytkownika ",
+    paste0("Osiem najpopularniejszych filmów i seriali dla użytkownika ",
            input$Osoba," w latach ", input$Zakres[1]," - ",input$Zakres[2]," to:")
     })
     
@@ -240,14 +276,33 @@ server <- function(input, output) {
              input$Osoba," w latach ", input$Zakres[1]," - ",input$Zakres[2],":")
     })
     
-    output$mapka <- renderPlot({
+    output$mapka <- renderPlotly({
+      kraje <- unique(exploring %>% filter(
+         exploring$Year >= input$Zakres[1],
+         exploring$Year <= input$Zakres[2],
+         Profile.Name == input$Osoba) %>% 
+         select(Country))
+      kraje_fixed <- eval(parse(text = (gsub("[A-Z]{2} \\((.*?)\\)", "\\1", kraje))))
+
       country %>% 
         ggplot(aes(x = long, y = lat, group = group)) + 
-        geom_polygon() +
+        geom_polygon(fill=ifelse(country$region %in% kraje_fixed, "red","black"), 
+                     color="white", size="0.1") +
         coord_fixed(1.3) +
-        theme_bw()
+        theme_void() 
     })
     
+    output$tabelka2 <- renderTable({
+      kraje <- unique(exploring %>% filter(
+        exploring$Year >= input$Zakres[1],
+        exploring$Year <= input$Zakres[2],
+        Profile.Name == input$Osoba) %>% 
+          select(Country))
+      kraje_fixed <- eval(parse(text = (gsub("[A-Z]{2} \\((.*?)\\)", "\\1", kraje))))
+      kraje_fixed
+    })
 }
 
 shinyApp(ui = ui, server = server)
+
+
